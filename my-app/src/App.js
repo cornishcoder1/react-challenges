ba4090ea5a8e69881922c260e22e25e54aea7333
cornishcoder1 <leahfisher07@hotmail.co.uk>
2022-06-29 14:57:24 +0000

    Handle events for login button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,15 @@ import css from './App.module.css';
 import Sidebar from './components/Sidebar';
 import NavBarSimple from './components/NavBarSimple';
 import EventsClass from './components/EventsClass';
+import NavBarForm from './components/NavBarForm';
 // import EventsClass from './components/EventsClass';
 // import EventsFunctional from './components/EventsFunctional';
 
 function App() {
   return (
     <div className={css.App}>
-      <NavBarSimple />
+      {/* <NavBarSimple /> */}
+      <NavBarForm />
       <Sidebar />
     </div>
   );
---
+++ my-app/src/components/NavBarForm.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import css from "./css/NavBarForm.module.css"
+
+export class NavBarForm extends Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        isLoggedIn: true,
+      }
+    }
+    handleButtonClick = () => {
+        this.setState((prevState) => ({
+            isLoggedIn: prevState.isLoggedIn ? false: true
+        }), () => console.log(this.state.isLoggedIn))
+    }
+   
+
+  render() {
+    return (
+      <div className={css.NavBar}>
+        <h1>My Gallery</h1>
+            {this.state.isLoggedIn ?
+                <button onClick={() => this.handleButtonClick()}>Log In</button>
+                :
+                <form>
+                    <label htmlFor='username'>Username:</label>
+                    <input placeholder='username' id='username'></input>
+                    <label htmlFor='password'>Password:</label>
+                    <input placeholder='password' id='password'></input>
+                    <button onClick={() => this.handleButtonClick()}>Submit</button>
+                </form>
+            }    
+      </div>
+    )
+  }
+}
+
+export default NavBarForm
